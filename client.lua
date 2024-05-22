ff0dc61e9de057f25416c3574e2ca146e977bd99
Lukk <162629758+Lukkay@users.noreply.github.com>
2024-05-22 19:13:34 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,9 +1,10 @@
 -- Made by Skyficek
+local RPC_NAME = "JMENO_RPC"
+local app_id = "ID_APLIKACNIHO_BOTA"
 
 addEventHandler("onClientResourceStart", resourceRoot, function()
-  local app_id = "ID_APLIKACNIHO_BOTA"
   if setDiscordApplicationID(app_id) then 
-   setDiscordRichPresenceAsset("JMENO_RPC", "JMENO_RPC")
+   setDiscordRichPresenceAsset(RPC_NAME, RPC_NAME)
    setDiscordRichPresenceButton(1, "🎮 Připojit se", "ZDE_ZADEJ_IP_SERVERU")
    setDiscordRichPresenceButton(2, "⚙️ Pozvánka na Discord", "ZDE_ZADEJ_POZVANKU_NA_DISCORD")
    local name = getPlayerName(localPlayer)
